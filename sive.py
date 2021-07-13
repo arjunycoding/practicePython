b3077eb39e31c4ee29ead0a364a48395a12282f4
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-07-13 16:25:56 -0400

    deleting sive2.py and mving it's conent into sive.py

--- sive.py
+++ sive.py
@@ -1,3 +1,4 @@
+# MY WAY
 print("Welcome! If you are having trouble finding if a number is prime or composite you've come to the right place!")
 number = int(input("Chosse a number and we will find out if it is prime or composite!\n"))
 possibleNumbers = []
@@ -18,3 +19,24 @@ def sive(num):
         print(f"{number} is a composite number")
 sive(number)
 print(f"Here is your all the factors for the number {number}:", factors)
+
+# PYTHON CORUSE WAY
+
+import math
+def sieve(num):
+    if num <= 2:
+        return False
+    else:
+        highestFactor = int(math.sqrt(num))
+        factors = []
+        for i in range(2, num + 1):
+            if i <= highestFactor or i == num:
+                for j in factors:
+                    if i % j == 0:
+                        break
+                else:
+                    factors.append(i)
+        lastNumber = factors.pop()
+        return lastNumber == num
+
+sieve(12)
