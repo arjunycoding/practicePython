aebd4c96f0db6be4e71bd3ac97442bfdc22e6003
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-08-12 18:01:20 -0400

    making function to draw tic tac toe borad

---
+++ tic-tac-toe.py
@@ -0,0 +1,40 @@
+# Print Board
+def printTTT(board):
+    if len(board) != 9:
+        return "Your list must be 9 numbers long"
+    
+    rowPos = 0
+    for i in range(3):
+        print("         |         |        ")
+        print("    ", end="")
+        if board[rowPos] == 1:
+            print("x", end="")
+        elif board[rowPos] == 2:
+            print("o", end="")
+        elif board[rowPos] == 0:
+            print(" ", end="")
+        print("    |", end="")
+
+        print("    ", end="")
+        if board[rowPos+1] == 1:
+            print("x", end="")
+        elif board[rowPos+1] == 2:
+            print("o", end="")
+        elif board[rowPos+1] == 0:
+            print(" ", end="")
+        print("    |", end="")
+
+        print("    ", end="")
+        if board[rowPos+2] == 1:
+            print("x", end="")
+        elif board[rowPos+2] == 2:
+            print("o", end="")
+        elif board[rowPos+2] == 0:
+            print(" ", end="")
+        print("    ")
+
+        print("         |         |        ")
+        print(" ---------------------------")
+        rowPos+=3
+
+printTTT([2,2,1,1,1,2,0,1,0])
